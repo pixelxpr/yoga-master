e09af0079124b1c44ba3e620e018dad416698b34
pixel xpr <pixelxpr@gmail.com>
2023-06-24 18:56:52 +0000

    updated app

--- components/Meta.js
+++ components/Meta.js
@@ -1,26 +1,34 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 const Meta = ({ title, keywords, description }) => {
   return (
     <Head>
-      <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <meta name='keywords' content={keywords} />
-      <meta name='description' content={description} />
-      <meta charSet='utf-8' />
-      <link rel='icon' href='/favicon.ico' />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="keywords" content={keywords} />
+      <meta name="description" content={description} />
+      <meta charSet="utf-8" />
+      <link rel="icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-      <link href="https://fonts.googleapis.com/css2?family=Teko:wght@400;500;600;700&display=swap" rel="stylesheet" />
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Teko:wght@400;500;600;700&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      />
       <title>{title}</title>
     </Head>
-  )
-}
+  );
+};
 
 Meta.defaultProps = {
-  title: 'shape up with ana',
-  keywords: 'yoga, workout, shape-up, shape up, exercise, strengthening, Calorie burners, Morning flow yoga, Morning yoga, Strength training, Vinyasa, Diet plans',
-  description: 'Workout has the power to change your life. Every individual is seeking to achieve different goals. My aim is to personalise your learning to maximise the physical and mental benefits that you are looking to achieve.',
-}
+  title: "shape up with ana",
+  keywords:
+    "yoga, workout, shape-up, shape up, exercise, strengthening, Calorie burners, Morning flow yoga, Morning yoga, Strength training, Vinyasa, Diet plans",
+  description:
+    "Workout has the power to change your life. Every individual is seeking to achieve different goals. My aim is to personalise your learning to maximise the physical and mental benefits that you are looking to achieve.",
+};
 
-export default Meta
+export default Meta;
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,7 @@
 import Layout from "../components/Layout";
 import Script from "next/script";
 
-import { clarity } from "react-microsoft-clarity";
-
 import "../styles/globals.css";
-
-clarity.init("hp0psh3rwp");
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -17,6 +13,16 @@ function MyApp({ Component, pageProps }) {
           console.error("Script failed to load", e);
         }}
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8126224721692056"
+      />
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "hp0psh3rwp");`,
+        }}
       />
       <Layout>
         <Component {...pageProps} />
