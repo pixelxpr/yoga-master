7ae1ff4a41d186b4a4cdac957f89bf18c9364b0e
pixel xpr <pixelxpr@gmail.com>
2023-06-24 18:46:26 +0000

    clarity

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,28 @@
-import Layout from '../components/Layout'
+import Layout from "../components/Layout";
 import Script from "next/script";
 
-import '../styles/globals.css'
+import { clarity } from "react-microsoft-clarity";
+
+import "../styles/globals.css";
+
+clarity.init("hp0psh3rwp");
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script id="Adsense-id" async onError={ (e) => { console.error('Script failed to load', e) }} src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8126224721692056" />
+      <Script
+        id="Adsense-id"
+        async
+        onError={(e) => {
+          console.error("Script failed to load", e);
+        }}
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8126224721692056"
+      />
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
