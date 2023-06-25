083ee6f00abf1ce2d2fea70d92296fcf1025451d
pixel xpr <pixelxpr@gmail.com>
2023-06-25 12:01:55 +0000

    footer link

--- components/Footer.js
+++ components/Footer.js
@@ -1,21 +1,21 @@
 // Global components
-import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
-import { useState, useEffect } from 'react';
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { useState, useEffect } from "react";
 
 // Local components
-import { db } from '../firebase/initFirebase';
+import { db } from "../firebase/initFirebase";
 
 // Styles
-import styles from '../styles/Footer.module.css'
+import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
-  const [footerText, setFooter] = useState('');
+  const [footerText, setFooter] = useState("");
 
   useEffect(() => {
-    const collectionRef = collection(db, 'footer');
+    const collectionRef = collection(db, "footer");
     const q = query(collectionRef);
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      setFooter(querySnapshot.docs.map(doc => doc.data()))
+      setFooter(querySnapshot.docs.map((doc) => doc.data()));
     });
 
     return unsubscribe;
@@ -23,9 +23,17 @@ const Footer = () => {
 
   return (
     <div className={styles.footer}>
-      <div>{footerText ? footerText?.map((item) => item.copyright) : ''}</div>
+      <div>{footerText ? footerText?.map((item) => item.copyright) : ""}</div>
+      <div>
+        Developed by
+        <Link href="mailto:pixelxpr@gmail.com">
+          <a target="_blank" rel="noopener noreferrer" className="link-item">
+            Pixelxpr
+          </a>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Footer;
